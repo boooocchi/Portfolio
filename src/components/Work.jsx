d0247b1f6bf4b61ab2ec5f3eebe99f951902fb0a
Kota Ito <boooocchi@gmail.com>
2023-07-09 17:46:48 -0700

    added more skills and edited a bit of responsive design

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import work from "@/assets/project.jpg";
 import { motion, useScroll } from "framer-motion";
-import smartRecipe from "../assets/smartRecipe.jpg";
 
 const Work = () => {
   return (
